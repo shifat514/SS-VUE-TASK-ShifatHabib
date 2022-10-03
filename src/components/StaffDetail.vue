60d7bd1035efd12899e820d5bffa515fc2b7e50c
shifat514 <shifat514@gmail.com>
2022-10-03 09:15:31 +0600

    Proper Validations Done

--- src/components/StaffDetail.vue
+++ src/components/StaffDetail.vue
@@ -1,24 +1,31 @@
 <template>
   <v-container>
-    <v-layout row wrap ma-3 justify-content-center align-self-center >
-        <v-flex xs6 >
+    <v-layout col wrap ma-3 justify-content-center align-self-center >
+        <v-flex xs5 sm6 col>
             <v-card>
-                <v-card-title>
-                    <h3 class="primary--text">{{staff.name}}</h3>
-                </v-card-title>
-                <v-img width="300px" class="ma-2"
-                          :src="staff.image"
-                        >
-                        </v-img>
+              <v-layout>
+                <v-flex pa-5>
+                  <v-img xs1 max-width="300px" :src="staff.image"></v-img>
+                </v-flex>
+                
+                <v-flex class="text-left">
+                  <v-card-title  ></v-card-title>
+                  <h3 class="white--text">Name: {{staff.name}}</h3>
+                  <v-card-text class="text-left">
+                    <h3 class="white--text">Email: {{staff.email}}</h3>
+                  <h3 class="white--text">Phone Number{{staff.phone}}</h3>
+                  </v-card-text>
+                 
+                </v-flex>
+                
+              </v-layout>
+                
+
                  <v-card-text>
-            <div class="info--text">{{staff.email}}</div>
-            <div >{{staff.phone}}</div>
-            </v-card-text>
-            <v-card-actions>
-                <v-spacer></v-spacer>
-                <v-btn color="rgb(35, 40, 92)" class="white--text">Update</v-btn>
-                <v-btn color="rgb(35, 40, 92)" class="white--text">Delete</v-btn>
-            </v-card-actions>
+            
+            <v-btn elevation="4"  class="btnInfo" route to="/allstaffs" large mb-4>Go Back</v-btn>
+                 </v-card-text>
+                 
             </v-card>
         </v-flex>
     </v-layout>
@@ -28,6 +35,11 @@
 <script>
 export default {
     props:['index'],
+    methods:{
+        removeStaff(index){
+        this.$store.dispatch('removeStaff',index);
+      }
+    },
     computed:{
     staff(){
         return this.$store.state.staffList[this.index];
@@ -37,5 +49,11 @@ export default {
 </script>
 
 <style>
-
+.btnInfo{
+  background-color: rgb(234, 235, 249) !important;
+    }
+  .btnInfo:hover{
+  background-color: rgb(35, 40, 92) !important;
+  color:white;
+  }
 </style>
--- src/components/UpdateStaff.vue
+++ src/components/UpdateStaff.vue
@@ -15,8 +15,7 @@
                   name="name"
                   label="Enter Name"
                   id="name"
-                  v-model="StaffList.name"
-                  required
+                  v-model="staffList.name"
                   >     
                   </v-text-field>
               </v-flex>
@@ -27,8 +26,9 @@
                   name="email"
                   label="Enter Email"
                   id="email"
-                  v-model="StaffList.email"
+                  v-model="staffList.email"
                 >
+                
             </v-text-field>
               </v-flex>
               </v-layout>
@@ -38,16 +38,16 @@
                   name="phone"
                   label="Enter phone number"
                   id="phone"
-                  v-model="StaffList.phone"
+                  v-model="staffList.phone"
                 ></v-text-field>
 
                 <v-text-field
                   name="image"
                   label="Enter image"
                   id="image"
-                  v-model="StaffList.image"
+                  v-model="staffList.image"
                 ></v-text-field>
-                <v-btn @click="addStaffs" class="btnInfo">add</v-btn>
+                <v-btn @click="verifyStaff" class="btnInfo">update</v-btn>
               </v-flex>
             </v-layout>
             </form>
@@ -60,39 +60,66 @@
   <script>
   export default {
     props:['index'],
-      computed:{
-      StaffList(){
-         return this.$store.state.staffList[this.index]
+      
+      methods:{
+        verifyStaff(){
+          
+        const isName=this.$store.state.staffList[this.index].name;
+        const isEmail=this.$store.state.staffList[this.index].email;
+        const isNumber=this.$store.state.staffList[this.index].phone;
+        const isImage=this.$store.state.staffList[this.index].image;
+        
+        const regexEmail = new RegExp('[a-z0-9]+@[a-z]+\\.[a-z]{2,3}');
+        const emailTesting=regexEmail.test(isEmail);
+
+        const regexName = new RegExp('^[A-Za-z]+(\\s*[A-Za-z]+)*$');
+        const nameTesting=regexName.test(isName);
+
+        const regexNumber=new RegExp('(^(\\+8801|8801|01))[1|3-9]{1}(\\d){8}$');
+        const numberTesting=regexNumber.test(isNumber);
+      
+      if(nameTesting==true )
+      {
+        if(emailTesting==true)
+        {
+          if(numberTesting==true)
+          {
+            if(isImage !== null && isImage !== '') 
+            {
+              alert('Staff is Updated!!');
+              this.$router.push('/allstaffs/');
+            }
+            else
+            {
+              alert('Add image Url!!');
+            }
+         
+          }
+          else{
+            alert('Phone Number is either invalid or empty! Add Bangladeshi Phone Number.');
+          }
+          
+        }
+        else{
+          alert('Email is either invalid or empty!');
+        }
+       
+      }
+      else{
+        alert('Name is either invalid or empty!');
+      }
+
     },
-    methods:{
-        
-    }
-   
+      
       },
-    
-        // addStaffs(){
-        //   console.log(this.name);
-        //   const staffData={
-        //     name: this.name,
-        //     email: this.email,
-        //     phone:this.phone
-        //   };
-        //   console.log(staffData.name);
-        //   this.$store.dispatch('addStaffs',staffData);
-        // },
-  
-        // removeStaff(index){
-        //    this.$store.dispatch('removeStaff',index);
-        // }
+      computed:{
+        staffList(){
+          return this.$store.state.staffList[this.index];
+          },
         
+      },
   }
   </script>
   
   <style>
-  ul{
-  
-    list-style-type:none;
-  }
-  
-  
   </style>
